5e4708f7fb05bb46b7e706e955f521a7b2d89564
RaisUpDeWold <raisupdewold@gmail.com>
2017-07-12 01:40:05 +0400

    Completed Static company website.

--- js/navigationbar.js
+++ js/navigationbar.js
@@ -1,22 +1,46 @@
 var bottom = $('.topnav').offset().top;
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+$(document).scroll(function() {
   if ($(this).scrollTop() > bottom) {
     $('.topnav').addClass('navbar-fixed-top');
     $('.topnav').addClass('top-nav-collapse');
+
+    $('#navigationSocialButtonGroup').css('display', 'inline-block');
   } else {
     $('.topnav').removeClass('navbar-fixed-top');
     $('.topnav').removeClass('top-nav-collapse');
+
+    $('#navigationSocialButtonGroup').css('display', 'none');
   }
+
+  setActiveListElements();
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-  $(document).on('click', 'a.page-scroll', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-  });
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top + 10
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
+
+// Update current item class
+function setActiveListElements(event){
+  var windowPos = $(document).scrollTop();
+  $('#topNavBar li a[href^="#"]').each(function() {
+    var currentLink = $(this);
+    if ($(currentLink.attr("href")).length > 0) {
+      var refElement = $(currentLink.attr("href"));
+      if (refElement.position().top <= windowPos && (refElement.position().top + refElement.height() + $("#topNavBar").height() ) > windowPos) {
+        $('#topNavBar li').removeClass("active");
+        currentLink.parent().addClass('active');
+      }
+      else{
+        currentLink.removeClass("active");
+      }
+    }
+  });
+}
